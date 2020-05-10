07bd8149b611d5edec973fb993c03452ca00d5f5
Łukasz Anuszkiewicz <odysey84@o2.pl>
2020-05-10 19:16:19 +0200

    Add mathematical notation in result

--- public/js/app.js
+++ public/js/app.js
@@ -7,17 +7,35 @@ document.addEventListener(
     const inputElemOne = document.getElementById("input-one");
     const inputElemTwo = document.getElementById("input-two");
     const resultElem = document.getElementById("result");
+    const prettyPrint = document.getElementById("pretty-print");
+    const resultBox = document.getElementById("result-box");
 
     formElem.addEventListener("submit", (e) => {
       e.preventDefault();
+      resultBox.style.display = 'block';
 
-      resultElem.textContent = JSON.stringify(adder(
-        eval(inputElemOne.value),
-        eval(inputElemTwo.value)
-      ), null, 4);
+      const result = adder(eval(inputElemOne.value), eval(inputElemTwo.value));
+      resultElem.textContent = JSON.stringify(result, null, 4);
+
+      prettyPrint.innerHTML = result
+        .map((term, index, expression) => {
+          const constant = term[0];
+          const exponential = term[1];
+
+          return `<span>${constant > 1 ? constant : ""}${
+            exponential >= 1 || exponential < 0 ? "x" : ""
+          }<sup>${
+            (exponential && exponential > 1) || (exponential && exponential < 0)
+              ? exponential
+              : ""
+          }</sup>${index !== expression.length - 1 ? "+" : ""}</span>`;
+        })
+        .join(" ");
     });
 
-    formElem.addEventListener("input", () => {});
+    formElem.addEventListener("input", () => {
+      resultBox.style.display = 'none';
+    });
   },
   false
 );

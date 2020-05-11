ced8b619a9138c3086c0c12ea6b90b63165600fd
Łukasz Anuszkiewicz <odysey84@o2.pl>
2020-05-11 09:01:16 +0200

    Add simpler and faster version

---
+++ public/js/adder_obj.js
@@ -0,0 +1,46 @@
+/**
+ *  Adder takes two arrays as arguments. Those arrays contains arrays
+ *    which have a structure in which each item has a specific
+ *    meaning:
+ *
+ *    constrain - number
+ *    exponential - number
+ *    term - [ constrain, exponential ]
+ *    expression - [ term, ... ]
+ *
+ *  ex:
+ *   2x^2 + 3 = [ [2, 2], [3] ]
+ *   3x^3 + x^2 = [ [3,3], [1,2] ]
+ *
+ * The return value is an array of same structure as arguments.
+ *
+ * @param {array[]} expressionOne - as described above
+ * @param {array[]} expressionTwo - as described above
+ *
+ * @return {object[]} - each key is Exponential (and c when term without Variable - constant term) and value is Constant
+ */
+
+export const expressionAdder = (expressionOne, expressionTwo) => {
+  let inputTerms = expressionOne.concat(expressionTwo),
+    resultExpression = {};
+
+  let length = inputTerms.length;
+  while (length) {
+    const [
+      inputTermsItemConstant,
+      inputTermsItemExponential = "c",
+    ] = inputTerms[length - 1];
+    const addedValue =
+      (resultExpression[inputTermsItemExponential] || 0) +
+      (inputTermsItemConstant || 0);
+
+    if (addedValue) {
+      resultExpression[inputTermsItemExponential] = addedValue;
+    } else {
+      delete resultExpression[inputTermsItemExponential];
+    }
+    length--;
+  }
+
+  return resultExpression;
+};
